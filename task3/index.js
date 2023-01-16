1ec667dacf7bd9c7000f7606977d0026b1867f7e
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-16 22:15:59 +0200

    added task3

--- task3/index.js
+++ task3/index.js
@@ -7,3 +7,17 @@
 // let arr = [5, 3, 8, 1];
 // filterRangeInPlace(arr, 1, 4); // видалені числа поза діапазоном 1..4
 // alert(arr ); // [3, 1]
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    let el = arr[i];
+    // console.log(el);
+    if (el < a || el > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+let arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4);
+console.log(arr);
