5ee2a3871b972c3b3927769176c2c3cf57833734
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-16 23:07:19 +0200

    added task7

--- task7/index.js
+++ task7/index.js
@@ -2,9 +2,22 @@
 // Ви маєте масив об'єктів user, і в кожному з них є user.name. Напишіть код, який
 // перетворює їх на масив імен.
 // Наприклад:
-// let vasya = { name: "Вася", age: 25};
-// let petya = { name: "Петя", age: 30};
-// let masha = { name: "Маша", age: 28};
-// let users = [ vasya, petya, masha ];
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+let users = [vasya, petya, masha];
 // let names = /* ... ваш код */
 // alert(names); // Вася, Петя, Маша
+
+console.log(users);
+function namearr() {
+  let map = new Map(Object.entries(users));
+  let names = [];
+  console.log(map);
+  map.forEach((value) => {
+    // console.log(value.name);
+    names.push(value.name);
+  });
+  console.log(names);
+}
+namearr();
