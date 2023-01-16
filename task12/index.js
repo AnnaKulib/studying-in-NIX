7bea247ff19604682651c5e98f38d7016655df54
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-16 21:47:30 +0200

    added task12

--- task12/index.js
+++ task12/index.js
@@ -2,13 +2,18 @@
 // Тут ми робимо два лічильники: counter і counter2, використовуючи ту саму функцію
 // makeCounter.
 // Вони незалежні? Що покаже другий лічильник? 0,1 чи 2,3 чи щось ще?
-// function makeCounter() {
-// let count = 0;
-// return function() {
-// return count++; };}
-// let counter = makeCounter();
-// let counter2 = makeCounter();
-// alert (counter ()); // 0
-// alert (counter ()); // 1
-// alert (counter2 ()); //?
-// alert (counter2 ()); //?
+function makeCounter() {
+  let count = 0;
+  return function () {
+    return count++;
+  };
+}
+let counter = makeCounter();
+let counter2 = makeCounter();
+
+console.log(counter()); // 0
+console.log(counter()); // 1
+console.log(counter2()); //0
+console.log(counter2()); //1
+
+// Функції незалежні. counter та counter2 відпрацьовують незалежно один від одної, створюючи свою змінну і опрацьовуючи її, видаючи результат.
