7e10f77d3ef2efb3a83c529dda2ff6ff676a9384
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-17 15:06:36 +0200

    added task6

--- task6/index.js
+++ task6/index.js
@@ -1,2 +1,28 @@
 // Завдання No 6. Створити калькулятор, що розширюється .
 // Створіть функцію Calculator, яка реалізує функціонал калькулятора.
+
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "*":
+      return num1 * num2;
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "/":
+      if (num2 !== 0) {
+        return num1 / num2;
+      } else {
+        return "не ділиться на нуль";
+      }
+  }
+}
+
+console.log(calculator(2, 0, "/"));
+
+// -------------------------------------------
+// помітки для себе:
+// В подальшому треба доробити можливість ввода користувачем декількох операцій,
+//   let num1 = parseFloat(prompt("Введіть число"));
+//   let operator = prompt("Введіть оператор  "+", "-", "*" чи "/"");
+//   let num2 = parseFloat(prompt("Введіть число"));
