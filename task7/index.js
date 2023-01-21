7cc7aa97dc0ea5672c1e62e84c0f4e1b12d56eae
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-21 15:33:17 +0200

    added task7

---
+++ task7/index.js
@@ -0,0 +1,11 @@
+// Завдання No7. Тег у коментарі
+
+// Що виведе код?
+// <script>
+// let body = document.body;
+// body.innerHTML = "<!--" + body.tagName + "-->";
+// alert( body.firstChild.data ); //?
+// </script>
+
+//Виведе BODY
+// data повертає вміст, а властивість .tagName виводиться в верхньому регістрі
