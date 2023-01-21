988b49aeeb48086f774c3b7a56b412f793e85611
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-21 15:00:11 +0200

    Update index.js

--- task4/index.js
+++ task4/index.js
@@ -16,12 +16,14 @@ let formByName = document.querySelector("form[name='search']");
 // console.log(document.querySelectorAll("#age-table label"));
 // td у цій таблиці (зі словом "Age "):
 // console.log(document.querySelector("#age-table td"));
+// console.log(tableById.getElementsByTagName("td")[0]);
 // console.log(tableById.firstElementChild.firstElementChild.firstElementChild); //жах, але працює
 // Форму form з ім'ям name = "search":
 // console.log(document.querySelector("form[name='search']"));
 // console.log(document.querySelector("form"));
 // console.log(document.body.firstElementChild);
 // Перший input у цій формі:
+// console.log(formByName.querySelector("input"));
 // console.log(formByName.firstElementChild.firstElementChild);
 // Останній input у цій формі:
 // console.log(formByName.lastElementChild);
