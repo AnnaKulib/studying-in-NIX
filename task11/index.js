06764ddd33584a33626a78b3bd129178050de768
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-23 22:35:29 +0200

    added just assignments

---
+++ task11/index.js
@@ -0,0 +1,13 @@
+// Завдання No11. Додайте пункт до списку, що випадає
+
+// Є <select>:
+
+// <select id="genres">
+// <option value="rock">Рок</option>
+// <option value="blues" selected>Блюз</option>
+// </select>
+
+// Використовуючи JavaScript:
+// 1. Виведіть значення та текст вибраного пункту.
+// 2. Додайте пункт: <option value="classic">Класика</option>.
+// 3. Зробіть його вибраним.
