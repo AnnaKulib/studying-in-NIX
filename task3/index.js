33c83ae2d4d9eafde3b0a84c615516075d78c7e1
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-24 18:53:45 +0200

    added task3

--- task3/index.js
+++ task3/index.js
@@ -6,3 +6,23 @@
 // Координати, обчислені вами, повинні збігатися з тими, які повертаються на кліку миші.
 // P.S. Код повинен працювати, якщо елемент має інші розміри або має рамку, без
 // прив'язки до конкретних чисел.
+
+let field = document.querySelector("#field");
+let coords = document.querySelector("#coords");
+let elem = field.getBoundingClientRect();
+
+document.onclick = function (e) {
+  // показує координати точки кліка;
+  coords.innerHTML = e.clientX + ":" + e.clientY;
+};
+
+let coordsCornerLeftTop = `${elem.left}: ${elem.top}`;
+let coordsCornerRightBottom = `${elem.right}: ${elem.bottom}`;
+let coordsCornerLeftTopInternal = `${elem.left + field.clientLeft}:
+  ${elem.top + field.clientTop}`;
+let coordsRightBottomInternal = `${elem.right - field.clientLeft}: 
+${elem.bottom - field.clientTop}`;
+console.log(coordsCornerLeftTop);
+console.log(coordsCornerRightBottom);
+console.log(coordsCornerLeftTopInternal);
+console.log(coordsRightBottomInternal);
