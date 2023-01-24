b6221e2ac3055bc859e4e00ed015fb5e4d818f58
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-24 14:44:29 +0200

    added task2

--- task2/index.js
+++ task2/index.js
@@ -11,3 +11,42 @@
 // html : "Hello!" , // HTML-повідомлення
 // className : "welcome" // Додатковий клас для div (необов'язково)
 // });
+
+function showNotification(params) {
+  let div = document.createElement("div");
+  div.classList.add("notification");
+
+  for (const param of Object.entries(params)) {
+    // console.log(param);
+    // console.log(param[0]);
+    // console.log(typeof param[1]);
+    switch (param[0]) {
+      case "html":
+        div.textContent = param[1];
+        break;
+      case "className":
+        div.classList.add(param[1]);
+        break;
+      default:
+        div.style[`${param[0]}`] = `${param[1]}`;
+        break;
+    }
+    if (typeof param[1] === "number") {
+      div.style[`${param[0]}`] = `${param[1]}px`;
+    }
+  }
+  document.body.append(div);
+
+  setTimeout(() => div.remove(), 1500);
+}
+
+showNotification({
+  top: 10, // 10px від верхньої межі вікна (за замовчуванням 0px)
+  left: 10, // 10px від правого краю вікна (за замовчуванням 0px)
+  html: "Hello!", // HTML-повідомлення
+  className: "welcome", // Додатковий клас для div (необов'язково)
+  backgroundColor: "red",
+  position: "absolute",
+  fontSize: 26,
+  width: 80,
+});
