4df781b0c45621045bfda8c1091802002117455b
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-24 13:45:47 +0200

    added task1

--- task1/index.js
+++ task1/index.js
@@ -7,4 +7,36 @@
 // понеділок.
 // Наприклад, createCalendar(cal, 2012, 9) згенерує в cal наступний календар:
 
-// PS У цьому завдання достатньо
+let cal = document.querySelector("#calendar");
+function createCalendar(elem, year, month) {
+  // користувач задає місяць числом 1 - 12,а в js місяці ідуть за індексами 0 - 11, тому  month - 1
+  let currentDate = new Date(year, month - 1, 1);
+  let finalDateOfMonth = new Date(year, month, 0);
+  //   console.log(new Date());
+  //   console.log(currentDate);
+  //   console.log(currentDate.getDate());
+  //   console.log(finalDateOfMonth);
+  // пошук понеділка. "допоки не понеділок"
+  while (currentDate.getDay() != 1) {
+    currentDate.setDate(currentDate.getDate() - 1);
+  }
+  let table = document.createElement("table");
+  table.innerHTML =
+    "<tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>нд</th></tr>";
+  while (currentDate <= finalDateOfMonth) {
+    let tr = document.createElement("tr");
+    for (let i = 0; i < 7; i++) {
+      let td = document.createElement("td");
+      if (currentDate.getMonth() == month - 1) {
+        td.textContent = currentDate.getDate();
+      }
+      td.textContent = currentDate.getDate();
+      tr.append(td);
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    table.append(tr);
+  }
+  elem.append(table);
+}
+
+createCalendar(cal, 2023, 2);
