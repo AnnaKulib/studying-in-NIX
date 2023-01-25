be37516c68a86fcc7a5e2da2e74bbc9348c64d76
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-25 14:17:45 +0200

    added task6

--- task6/index.js
+++ task6/index.js
@@ -13,3 +13,50 @@
 //     positionAt(blockquote , " top - out " , note );
 //     // показує нотатку всередині цитати поблизу верхнього краю елемента
 //     positionAt(blockquote , "top-in" , note );
+
+function positionAt(anchor, position, elem) {
+  elem.style.position = "absolute";
+  let coords = anchor.getBoundingClientRect();
+  switch (position) {
+    case "top-out":
+      elem.style.top = `${coords.top - elem.offsetHeight}px`;
+      elem.style.left = `${coords.left}px`;
+      break;
+    case "right-out":
+      elem.style.top = `${coords.top}px`;
+      elem.style.left = `${coords.right}px`;
+      break;
+    case "bottom-out":
+      elem.style.top = `${coords.top + anchor.offsetHeight}px`;
+      elem.style.left = `${coords.left}px`;
+      break;
+    case "top-in":
+      elem.style.top = `${coords.top}px`;
+      elem.style.left = `${coords.left}px`;
+      break;
+    case "right-in":
+      elem.style.top = `${coords.top}px`;
+      elem.style.left = `${coords.right - elem.offsetWidth}px`;
+      break;
+    case "bottom-in":
+      elem.style.top = `${
+        coords.top + anchor.offsetHeight - elem.offsetHeight
+      }px`;
+      elem.style.left = `${coords.left}px`;
+      break;
+  }
+}
+
+function showNote(anchor, position, html) {
+  let note = document.createElement("div");
+  note.className = "note";
+  note.innerHTML = html;
+  document.body.append(note);
+
+  positionAt(anchor, position, note);
+}
+
+let blockquote = document.querySelector("blockquote");
+
+showNote(blockquote, "right-out", "note right-out");
+showNote(blockquote, "top-in", "note top-in");
