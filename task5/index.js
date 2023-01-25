e16d922ec1640fd1bc87ff3fc30fb59f2f35c04f
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-25 13:51:20 +0200

    added task5

--- task5/index.js
+++ task5/index.js
@@ -5,3 +5,39 @@
 // Використовуйте рішення попереднього завдання для початку.
 // Щоб перевірити рішення в умовах прокручування, додайте стиль елементу <body style =
 // "height: 2000px "> .
+
+function positionAt(anchor, position, elem) {
+  //   elem.style.position = "fixed";
+  elem.style.position = "absolute";
+  let coords = anchor.getBoundingClientRect();
+  switch (position) {
+    case "top":
+      elem.style.top = `${coords.top - elem.offsetHeight}px`;
+      elem.style.left = `${coords.left}px`;
+      break;
+    case "right":
+      elem.style.top = `${coords.top}px`;
+      elem.style.left = `${coords.right}px`;
+      break;
+    case "bottom":
+      elem.style.top = `${coords.top + anchor.offsetHeight}px`;
+      elem.style.left = `${coords.left}px`;
+      break;
+  }
+}
+// Показує нотатку із заданим вмістом на заданій позиції Щодо елемента anchor.
+function showNote(anchor, position, html) {
+  let note = document.createElement("div");
+  note.className = "note";
+  note.innerHTML = html;
+  document.body.append(note);
+
+  positionAt(anchor, position, note);
+}
+
+// test it
+let blockquote = document.querySelector("blockquote");
+
+showNote(blockquote, "top", "note above");
+showNote(blockquote, "right", "note at the right");
+showNote(blockquote, "bottom", "note below");
