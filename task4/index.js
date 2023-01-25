0c1e263b849a9089cc57df6245c1c06ab30025a8
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-25 13:41:40 +0200

    added task4

--- task4/index.js
+++ task4/index.js
@@ -9,3 +9,45 @@
 // Вона використовується всередині функції showNote(anchor , position , html) , яка вже є у
 // вихідному коді завдання. Вона створює і показує елемент-«примітку» з текстом html на
 // заданій позиції position поряд з елементом anchor.
+
+//  Позиціонує елемент elem щодо елемента anchor відповідно до значення position.
+
+//  @param {Node} anchor елемент, біля якого позиціонується інший
+// елемент
+//  @param {string} position одне з: top/right/bottom
+//  @param {Node} elem елемент, який позиціонується
+// Обидва елементи elem і anchor повинні бути присутніми у документі
+function positionAt(anchor, position, elem) {
+  elem.style.position = "absolute";
+  let coords = anchor.getBoundingClientRect();
+  switch (position) {
+    case "top":
+      elem.style.top = `${coords.top - elem.offsetHeight}px`;
+      elem.style.left = `${coords.left}px`;
+      break;
+    case "right":
+      elem.style.top = `${coords.top}px`;
+      elem.style.left = `${coords.right}px`;
+      break;
+    case "bottom":
+      elem.style.top = `${coords.top + anchor.offsetHeight}px`;
+      elem.style.left = `${coords.left}px`;
+      break;
+  }
+}
+// Показує нотатку із заданим вмістом на заданій позиції Щодо елемента anchor.
+function showNote(anchor, position, html) {
+  let note = document.createElement("div");
+  note.className = "note";
+  note.innerHTML = html;
+  document.body.append(note);
+
+  positionAt(anchor, position, note);
+}
+
+// test it
+let blockquote = document.querySelector("blockquote");
+
+showNote(blockquote, "top", "note above");
+showNote(blockquote, "right", "note at the right");
+showNote(blockquote, "bottom", "note below");
