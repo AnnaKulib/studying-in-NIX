975012482ef12543ba476d30519479cde087a630
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-25 15:06:13 +0200

    added task8

--- task8/index.js
+++ task8/index.js
@@ -11,3 +11,27 @@
 // будь-яких фіксованих значень.
 // Використовуйте властивості event.clientX / event.clientY для визначення координат миші
 // при натисканні.
+
+let field = document.getElementById("field");
+let ball = document.getElementById("ball");
+
+field.addEventListener("click", (event) => {
+  let heightField = field.clientHeight;
+  let heightN = field.offsetTop + field.clientTop;
+  //   console.log(field.offsetTop, field.clientTop);
+  let widthField = field.clientWidth;
+  let widthN = field.offsetLeft + field.clientLeft;
+  //   console.log(field.offsetLeft, field.clientLeft);
+  let y = event.clientY - heightN - ball.offsetHeight / 2;
+  let x = event.clientX - widthN - ball.offsetWidth / 2;
+  //   console.log(
+  //     `heightField:${heightField}, heightN:${heightN}, widthField:${widthField}, widthN:${widthN}, y:${y}, x:${x}`
+  //   );
+
+  if (y < 0) y = 0;
+  if (x < 0) x = 0;
+  if (y > heightField - ball.offsetHeight) y = heightField - ball.offsetHeight;
+  if (x > widthField - ball.offsetWidth) x = widthField - ball.offsetWidth;
+  ball.style.top = y + "px";
+  ball.style.left = x + "px";
+});
