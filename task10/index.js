bef7d30f25d364449918e69223a56ee9a26bebd5
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-25 15:57:15 +0200

    added task10

--- task10/index.js
+++ task10/index.js
@@ -2,3 +2,13 @@
 // Є список повідомлень.
 // За допомогою JavaScript для кожного повідомлення додайте у верхній правий кут кнопку
 // закриття.
+
+let panes = document.querySelectorAll(".pane");
+
+for (const pane of panes) {
+  pane.insertAdjacentHTML(
+    "afterbegin",
+    '<button class="remove-button">[x]</button>'
+  );
+  pane.firstChild.onclick = () => pane.remove();
+}
