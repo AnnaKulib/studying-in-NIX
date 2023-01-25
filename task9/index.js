23fe9bb954d690716182d9180aa5bb6667912f3e
Anna Kulibabina <anna.kulibabina@gmail.com>
2023-01-25 15:44:59 +0200

    added task9

--- task9/index.js
+++ task9/index.js
@@ -1,3 +1,10 @@
 // Завдання No 9. Створити меню, що розкривається.
 // Створити меню, яке натискання відкривається або закривається.
 // P. S. HTML / CSS вихідного документа можна і потрібно міняти.
+
+let dropdownMenu = document.querySelector(".dropdown");
+let dropdownMenuText = document.querySelector(".text");
+
+dropdownMenuText.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("open");
+});
